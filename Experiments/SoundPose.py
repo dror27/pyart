fa25de7fd51e8645014aa57a3af87173b404363d
Dror Kessler <dror27.kessler@gmail.com>
2022-03-30 20:25:20 +0300

    scale argument

--- Experiments/SoundPose.py
+++ Experiments/SoundPose.py
@@ -7,18 +7,30 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("--camera", help="camera to use (0, 1, etc)",
                     type=int, default=0)
+parser.add_argument("--scale", help="name of scale to use, defaults to minor_pentatonic",
+                    default="minor_pentatonic")
+parser.add_argument("--list-scales", help="list all available scales",
+                    action="store_true")
 args = parser.parse_args()
 
 
-# generate note frequencies
-all_notes = []
-for i in range(12 + 13):
-    f = 110 * (2 ** (i/12))
-    all_notes.append(f)
-print(all_notes)
+# print scales
+from musthe import *
+if args.list_scales:
+    for s in list(dict.fromkeys([s.name for s in Scale.all(include_greek_modes=True)])):
+        print(s)
+    quit()
 
-# slice a pendatonic scale
-notes = [all_notes[i] for i in [0, 3, 5, 7, 10, 12, 15, 17, 19, 22, 24]]
+# generate scales
+def multi_octave_scale(note, name, octaves):
+    if octaves == 0:
+        return [note]
+    else:
+        s = Scale(note, name)
+        return [s[i] for i in range(len(s))] + multi_octave_scale(note + Interval('P8'), name, octaves - 1)
+
+scale1 = multi_octave_scale(Note('A2'), args.scale, 2)
+scale2 = multi_octave_scale(Note('A3'), args.scale, 2)
 
 
 # start sound engine
@@ -29,30 +41,32 @@ beat = Beat(time=.100, taps=16, w1=[90,80], w2=50, w3=35, poly=1).play()
 tr2 = TrigEnv(beat, table=t, dur=beat['dur'], mul=beat['amp'])
 
 lfo4 = Sine(0.1).range(0.1, 0.75)
-osc4 = SuperSaw(freq=notes[0], detune=lfo4, mul=tr2*0.3).out(1)
+osc4 = SuperSaw(freq=scale1[0].frequency(), detune=lfo4, mul=tr2*0.3).out(1)
 lfo5 = Sine(0.1888).range(0.1, 0.75)
-osc5 = SuperSaw(freq=notes[5], detune=lfo5, mul=tr2*0.3).out(2)
+osc5 = SuperSaw(freq=scale2[0].frequency(), detune=lfo5, mul=tr2*0.3).out(2)
 sf = SfPlayer("kick-02.wav", speed=1, loop=True, mul=0.5).out()
 
 
 # define motion processors
-def y_freq(y):
+def y_note(y, scale):
     y = min(1.0, max(0.0, y))
-    return notes[int((1 - y) * (len(notes) - 1))]
+    return scale[int((1 - y) * (len(scale) - 1))]
 
 def process_landmark(landmark):
 
     y1 = landmark[19].y
     y2 = landmark[20].y
 
-    f1 = y_freq(y1)
-    f2 = y_freq(y2)*2
+    n1 = y_note(y1, scale1)
+    n2 = y_note(y2, scale2)
 
-    osc4.setFreq(f1)
-    osc5.setFreq(f2)
+    osc4.setFreq(n1.frequency())
+    osc5.setFreq(n2.frequency())
 
-    return {"f1":f1, "f2":f2}
+    return {"n1":note_info(n1), "n2":note_info(n2)}
     
+def note_info(note):
+    return "%s %.1f" % (note.scientific_notation(), note.frequency()) 
 
 # start pose detection
 import cv2
@@ -102,7 +116,7 @@ with mp_pose.Pose(
 
     # put text
     y = 50
-    for text in ["%s: %.1f" % (key, info[key]) for key in info]:
+    for text in ["%s: %s" % (key, str(info[key])) for key in info]:
         cv2.putText(image, text, (10, y), cv2.FONT_HERSHEY_SIMPLEX, 
                    1, (0,0,255), 2, cv2.LINE_AA)
         y += 40
@@ -115,7 +129,7 @@ with mp_pose.Pose(
 cap.release()
 
 s.stop()
-
+quit()
 
 
 
